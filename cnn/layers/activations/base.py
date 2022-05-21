13ed2e02d9194e8f22da805841191a273f14228b
JamesQuirk <james1201019@gmail.com>
2022-05-21 19:02:08 +0100

    feat: add wrapping forwards/ backwards methods for BaseActivation

--- cnn/layers/activations/base.py
+++ cnn/layers/activations/base.py
@@ -17,3 +17,27 @@ class BaseActivation(Layer):
 			self.INPUT_SHAPE = self.prev_layer.OUTPUT_SHAPE
 		self.OUTPUT_SHAPE = self.INPUT_SHAPE
 
+	def forwards(self, X):
+		if self.prev_layer.LAYER_TYPE == 'FC':
+			assert len(X.shape) == 2 and X.shape[0] == self.INPUT_SHAPE[0], f'Expected input of shape {self.INPUT_SHAPE} instead got {(X.shape[0],1)}'
+		self.input = X
+
+		self._forwards(X)
+
+		assert self.output.shape == X.shape, f'Output shape, {self.output.shape}, not the same as input shape, {X.shape}.'
+		self._track_metrics(output=self.output)
+
+		return self.output
+
+	def backwards(self, dCdA):
+		assert dCdA.shape == self.output.shape, f'dC/dA shape, {dCdA.shape}, not as expected, {self.output.shape}.'
+		self._track_metrics(cost_gradient=dCdA)
+
+		dCdZ = self._backwards(dCdA)
+
+		assert dCdZ.shape == self.prev_layer.output.shape, f'Back propagating dC_dZ has shape: {dCdZ.shape} when previous layer output has shape {self.prev_layer.output.shape}'
+
+		return dCdZ
+
+
+
